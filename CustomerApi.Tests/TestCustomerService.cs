257c4c4121c185a0399ab28cb7311af8cf0f6162
Kurt <klim049@gmail.com>
2018-08-16 22:35:01 +1000

    Add unit Test project to solution

---
+++ CustomerApi.Tests/TestCustomerService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomerApi.Tests
+{
+    [TestClass]
+    public class TestCustomerService
+    {
+        [TestMethod]
+        public void GetAllCustomers_ShouldReturnAllCustomers()
+        {
+        }
+
+        [TestMethod]
+        public void GetCustomer_ShouldReturnACustomer()
+        {
+        }
+
+        [TestMethod]
+        public void AddCustomer_ShouldAddACustomer()
+        {
+        }
+
+        [TestMethod]
+        public void UpdateCustomer_ShouldUpdateACustomer()
+        {
+        }
+
+        [TestMethod]
+        public void RemoveCustomer_ShouldRemoveACustomer()
+        {
+        }
+    }
+}
