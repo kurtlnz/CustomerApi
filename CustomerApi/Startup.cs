e494be81e4d17e969f584176b6c40b8277b5ed41
Kurt <klim049@gmail.com>
2018-08-18 12:36:56 +1000

    Added Swashbuckle to project and enabled Swagger UI

--- CustomerApi/Startup.cs
+++ CustomerApi/Startup.cs
@@ -1,35 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CustomerApi.Models;
+﻿using CustomerApi.Models;
 using CustomerApi.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace CustomerApi
 {
     public class Startup
     {
-        //public Startup(IConfiguration configuration)
-        //{
-        //    Configuration = configuration;
-        //}
-
-        //public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<CustomerContext>(opt => opt.UseInMemoryDatabase("CustomerList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddScoped<ICustomerService, CustomerService>();
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Kurt Lim - Customer API",
+                    Description = "Coding challenge for Acurus",
+                    //TermsOfService = "None",
+                    Contact = new Contact()
+                    {
+                        Name = "Kurt Lim",
+                        Email = "klim049@gmail.com",
+                        Url = "https://github.com/lim-code/"
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +43,10 @@ namespace CustomerApi
             }
 
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Customer API");
+            });
         }
     }
 }
